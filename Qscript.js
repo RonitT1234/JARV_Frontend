c543958e221f2a7e8c9435ce8f262699f172d102
VanceReynolds <vancenreynolds@gmail.com>
2023-10-31 14:45:56 -0700

    pokemon trivia quiz

--- Qscript.js
+++ Qscript.js
@@ -1,10 +1,5 @@
 const questions = [
     {
-        question: "What is the first Pokémon in the Pokédex?",
-        options: ["Bulbasaur", "Pikachu", "Charizard", "Squirtle"],
-        answer: "Bulbasaur",
-    },
-    {
         question: "What is the first Pokémon ever created?",
         options: ["Bulbasaur", "Charmander", "Pikachu", "Rhydon"],
         answer: "Rhydon",
@@ -13,6 +8,46 @@ const questions = [
         question: "Which type is strong against Water-type Pokémon?",
         options: ["Fire", "Electric", "Grass", "Psychic"],
         answer: "Grass",
+    },
+    {
+        question: "What device do trainers use to access a database of all their Pokemon?",
+        options: ["Pokelist", "Pokedex", "Pokemon Training Chart", "Poke-Catalog"],
+        answer: "Pokedex",
+    },
+    {
+        question: "As of October 2023, how many pokemon generations are there?",
+        options: ["6","7","9","13"],
+        answer: "9",
+    },
+    {
+        question: "Who gave Ash Ketchum his first pokemon?"
+        options: ["Professor Oak", "Nurse Joy", "Professor X", "Mr. Fox"],
+        answer: "Professor Oak",
+    },
+    {
+        question: "Who does Pikachu evolve into?",
+        options: ["Pichu", "Chuchu", "Pekuchi","Raichu"],
+        answer: "",
+    }
+    {
+        question: "What is the first Pokémon the Pokédex? (Not first created)",
+        options: ["Bulbasaur", "Pikachu", "Charizard", "Squirtle"],
+        answer: "Bulbasaur",
+    },
+    {
+        question: "How do you make a ditto return to its normal form?",
+        options: ["Make it cry", "Give it food", "Beat it in a battle", "Make it laugh"],
+        answer: "Make it laugh",
+    },
+    {
+        question: "What Pokemon was Ash's first catch?",
+        options: ["Pikachu","Squirtle","Caterpie","Magicarp"],
+        answer: "Caterpie",
+    },
+    {
+        question: "Which of these Pokemon is a fairy type",
+        options: ["Jigglypuff", "Diglett", "Eevee", "Snorlax"],
+        answer: "Jigglypuff",
     },
     // Add more questions
 ];
---
+++ quiz.md
@@ -0,0 +1,19 @@
+---
+title: Quiz
+---
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="QuizStyle.css">
+</head>
+<body>
+    <div class="quiz-container">
+        <h1>Test Your Pokemon Knowledge</h1>
+        <p id="question"></p>
+        <div id="options"></div>
+        <button id="next-button" disabled>Next</button>
+        <button id="restart-button">Restart Quiz</button>
+        <p id="result"></p>
+    </div>
+    <script src="Qscript.js"></script>
+</body>
+</html>
